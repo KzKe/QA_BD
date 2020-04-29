473b6556828c607f93d0b8f2b825b3cd5276f0d7
KzKe <kangk1@student.unimelb.edu.au>
2020-04-29 15:22:38 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,3 +5,14 @@ from data import *
 train_examples = read_question_answer_examples(config.TRAINING_FILE)
 dev_examples = read_question_answer_examples(config.DEV_FILE)
 test_examples = read_question_answer_examples(config.TEST_FILE)
+
+
+train_features = convert_examples_to_features(train_examples, tokenizer=config.TOKENIZER)
+dev_features = convert_examples_to_features(dev_examples, tokenizer=config.TOKENIZER)
+test_features = convert_examples_to_features(test_examples, tokenizer=config.TOKENIZER)
+
+# temp = Question_Answer_Dataset(train_features)
+
+train_features = Question_Answer_Dataset(train_features)
+dev_features = Question_Answer_Dataset(dev_features)
+test_features = Question_Answer_Dataset(test_features)
